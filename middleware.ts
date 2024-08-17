00a03193e3cf60fc668fd9c049edfbb7bea054f9
manak1 <m_ikeda@yumemi.co.jp>
2024-08-17 15:08:29 +0900

    feat: 🐧 未ログイン状態の場合はsignInにリダイレクトするように変更

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { auth } from "@/src/libs/auth";
+
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname !== "/signIn") {
+    const newUrl = new URL("/signIn", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
