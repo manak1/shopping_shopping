79d29ddc53e189c7f3b26c194f76d89de3efe063
manak1 <m_ikeda@yumemi.co.jp>
2024-08-17 15:07:52 +0900

    chore: 🔧 prismaがedge上でも動作するように変更

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -3,7 +3,12 @@ import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
-const prisma = new PrismaClient();
+import { Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaNeon(neon);
+const prisma = new PrismaClient({ adapter });
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
