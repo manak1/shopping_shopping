c46333f291569052ee3a3398f7ebf21625e30ff2
manak1 <m_ikeda@yumemi.co.jp>
2024-08-17 14:25:55 +0900

    feat: 🐧 Googleログインができるところまで実装

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -1,10 +1,11 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
 
 const prisma = new PrismaClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [Google],
 });
