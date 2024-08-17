86be74411a30a209c9b1433d7f1d1f459714857f
manak1 <m_ikeda@yumemi.co.jp>
2024-08-17 13:56:45 +0900

    feat: 🐧 prisma schemaのセットアップ

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -1,5 +1,10 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 
+const prisma = new PrismaClient();
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [],
 });
